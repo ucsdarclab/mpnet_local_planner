50b91079eb19aff39e58ab9c44ae6b1ebd235ab0
jacobjj <jjj025@ucsd.edu>
2020-02-14 11:26:46 -0800

    added file to define the function

---
+++ src/mpnet_plan_ros.cpp
@@ -0,0 +1,11 @@
+// Describing different components of the local planner
+
+
+#include<mpnet_plan.h>
+
+namespace mpnet_local_planner{
+    void MpnetLocalPlanner::initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap)
+    {
+        std::cout << "Everything is fine";
+    }
+}
