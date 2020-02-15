fbcfe7e0a2db02190b2393d962fbe6802f185e87
jacobjj <jjj025@ucsd.edu>
2020-02-14 23:17:11 -0800

    Added new method to the class

--- include/mpnet_plan.h
+++ include/mpnet_plan.h
@@ -27,8 +27,8 @@ namespace mpnet_local_planner{
          * @param x The x co-ordinate of the robot
          * @param y The y co-ordinate of the robot
          * @param resolution The resolution of the costmap
-         * @param origin_x The x co-ordinate of the costmap origin
-         * @param origin_y The y co-ordinate of the costmap origin
+         * @param origin_x The x co-ordinate of the local costmap origin
+         * @param origin_y The y co-ordinate of the local costmap origin
          * @param costmap A pointer to the local costmap
          */
         torch::Tensor copy_costmap( double x, double y, double resolution, double origin_x, double origin_y, costmap_2d::Costmap2D* costmap);
@@ -38,12 +38,19 @@ namespace mpnet_local_planner{
          * @param start The starting position of the robot
          * @param goal The goal position the robot has to achieve
          * @param bounds The bounds of the robot
-         * @param origin_x The x co-ordinate of the costmap origin
-         * @param origin_y The y co-ordinate of the costmap origin
+         * @param origin_x The x co-ordinate of the local costmap origin
+         * @param origin_y The y co-ordinate of the local costmap origin
          */
         torch::Tensor copy_pose(const ob::ScopedState<> &start, const ob::ScopedState<> &goal, std::vector<double> bounds, double origin_x, double origin_y);
         
-        
+        /**
+         * @brief A function to return a vector given the target point predicted by the network
+         * @param target_state The target tensor returned by the network
+         * @param bounds The bound of the local costmap
+         * @param origin_x  The x co-ordinate of the local costmap origin
+         * @param origin_y The y co-ordinate of the local costmap origin
+         */
+        std::vector<double> getMapPoint(torch::Tensor target_state, std::vector<double> bounds, double origin_x, double origin_y);
         private:
         static char* cost_translation_table;
 
