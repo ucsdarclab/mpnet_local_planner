cfae68146a754fbb7c6d8b07009d6340a105ada0
jacobjj <jjj025@ucsd.edu>
2020-03-09 16:12:38 -0700

    increase bounds for planning

--- src/mpnet_plan.cpp
+++ src/mpnet_plan.cpp
@@ -100,10 +100,10 @@ namespace mpnet_local_planner{
             // bounds->setHigh(0,13.5);
             // bounds->setHigh(1,14.0);
 
-            bounds->setLow(0,-1.8);
-            bounds->setLow(1,-2.1);
-            bounds->setHigh(0,4.0);
-            bounds->setHigh(1,9.25);
+            bounds->setLow(0,-20);
+            bounds->setLow(1,-20);
+            bounds->setHigh(0,20);
+            bounds->setHigh(1,20);
             
             bounds->setLow(2,-M_PI);
             bounds->setHigh(2,M_PI);
