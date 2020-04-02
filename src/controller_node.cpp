a2678d9bad18012608fdb971476e0cd8ba6010aa
jacobjj <jjj025@ucsd.edu>
2020-04-02 09:54:48 -0700

    For simulation use /odom

--- src/controller_node.cpp
+++ src/controller_node.cpp
@@ -5,11 +5,12 @@ int main(int argc, char **argv)
 		ros::init(argc, argv, "controller");
 
 		ros::NodeHandle n;
-        // mpnet_local_planner::OdometryHelperRos odom_helper_("/odom");
-
-        mpnet_local_planner::OdometryHelperRos odom_helper_("/robot_pose_ekf/odom_ekf_topic");
-        // mpnet_local_planner::OdometryHelperRos odom_helper_("/robot_pose_ekf/odom_combined_ekf");
-
+        // For simulation use: 
+        mpnet_local_planner::OdometryHelperRos odom_helper_("/odom");
+        
+        // For real-world use:
+        // mpnet_local_planner::OdometryHelperRos odom_helper_("/robot_pose_ekf/odom_ekf_topic");
+        
 		mpnet_local_planner::Controller controller(false);
         geometry_msgs::PoseStamped robot_vel;
         ackermann_msgs::AckermannDriveStamped control_msg;
